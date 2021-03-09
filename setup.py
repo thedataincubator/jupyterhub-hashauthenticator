e5013db5896db0109d6883036972004cb4be20ce
Robert Schroll <rschroll@gmail.com>
2021-03-09 13:12:28 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-hashauthenticator',
-    version='0.4.0',
+    version='0.4.1',
     description='Hashed Password Authenticator for JupyterHub',
     url='https://github.com/thedataincubator/jupyterhub-hashauthenticator',
     author='Petko Minkov',
